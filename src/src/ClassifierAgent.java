51c5fd0aad7bd08e6d843091ca9653409b7fc18c
Víctor Giménez Ábalos <victor.gimenez.abalos@est.fib.upc.edu>
2019-11-05 18:59:26 +0100

    Added skeleton of simpleton ping agent for classifier agent. To work from there

--- src/src/ClassifierAgent.java
+++ src/src/ClassifierAgent.java
@@ -1,6 +1,71 @@
-import jade.core.Agent;
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.util.Logger;
 
 public class ClassifierAgent extends Agent {
 
+    private Logger myLogger = Logger.getMyLogger(getClass().getName());
 
+    private class WaitMSGAndActBehaviour extends CyclicBehaviour {
+
+        public WaitMSGAndActBehaviour(Agent a) {
+            super(a);
+        }
+
+        public void action() {
+            ACLMessage  msg = myAgent.receive(); // Wait for message
+            if(msg != null){
+                ACLMessage reply = msg.createReply();
+
+                if(msg.getPerformative()== ACLMessage.REQUEST){
+                    String content = msg.getContent();
+                    if ((content != null) && (content.indexOf("ping") != -1)){
+                        myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Received PING Request from "+msg.getSender().getLocalName());
+                        reply.setPerformative(ACLMessage.INFORM);
+                        reply.setContent("pong");
+                    }
+                    else{
+                        myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Unexpected request ["+content+"] received from "+msg.getSender().getLocalName());
+                        reply.setPerformative(ACLMessage.REFUSE);
+                        reply.setContent("( UnexpectedContent ("+content+"))");
+                    }
+
+                }
+                else {
+                    myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Unexpected message ["+ACLMessage.getPerformative(msg.getPerformative())+"] received from "+msg.getSender().getLocalName());
+                    reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
+                    reply.setContent("( (Unexpected-act "+ACLMessage.getPerformative(msg.getPerformative())+") )");
+                }
+                send(reply);
+            }
+            else {
+                block();
+            }
+        }
+    }
+
+
+    protected void setup() {
+        // Registration with the DF
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("ClassifierAgent");
+        sd.setName(getName());
+        sd.setOwnership("IMAS_group");
+        dfd.setName(getAID());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this,dfd);
+            WaitMSGAndActBehaviour PingBehaviour = new  WaitMSGAndActBehaviour(this);
+            addBehaviour(PingBehaviour);
+        } catch (FIPAException e) {
+            myLogger.log(Logger.SEVERE, "Agent "+getLocalName()+" - Cannot register with DF", e);
+            doDelete();
+        }
+    }
 }
